778e7ff22d6372f9ea303f3d8161ecc344588dcf
khamtaler <tsroka41@gmail.com>
2020-01-19 21:15:01 +0100

    menu update

---
+++ menu.js
@@ -0,0 +1,37 @@
+window.onload=function () {
+    var tabs = document.querySelector('.tabs')
+    var li = document.querySelectorAll('a')
+    var content = document.querySelectorAll('.section-content')
+
+    tabs.addEventListener("click", function(e){
+        e.preventDefault();
+        if (e.target && e.target.nodeName === 'A'){
+            for(var i = 0; i < li.length; i++){
+                li[i].classList.remove('active');
+            }
+            e.target.classList.toggle('active');
+
+            for(var i = 0; i < content.length; i++){
+                content[i].classList.remove('active');
+
+            }
+            var tabName = '#' + e.target.dataset.name;
+            document.querySelector(tabName).classList.toggle('active');
+        }
+
+
+    })
+
+    var editModal = document.querySelector('#edit-modal');
+    var editButton = document.querySelector('#edit-btn');
+    var closeButton = document.querySelector('#edit-close');
+
+    editButton.addEventListener('click', function () {
+        editModal.style.display = "block";
+    });
+    closeButton.addEventListener('click', function () {
+        editModal.style.display = "none";
+    });
+
+}
+
